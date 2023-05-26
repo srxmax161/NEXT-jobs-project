add01c7f757490f934f0a85312c9167d0048711c
LimShuonle <srxmax161@gmail.com>
2023-05-26 14:22:38 +0800

    initial commit

---
+++ src/routes/testproject/+layout.svelte
@@ -0,0 +1,11 @@
+<nav class='flex justify-between'>
+            <div>
+            <a href='./testproject'>DATABASE</a>
+            </div>
+            <div>
+            <a href='./testproject'>SIGNUP</a>
+            <a href='./about'>LOG IN</a>
+            </div>
+</nav>
+
+<slot />
