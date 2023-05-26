ff6c406dd1cc6e824bdb81ebc984e850e37a1614
LimShuonle <srxmax161@gmail.com>
2023-05-26 15:03:58 +0800

    implemented nav bar to pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
+<nav class='flex justify-between'>
+  <div>
+  <a href='/'>DATABASE</a>
+  </div>
+  <div>
+  <a href='/users/new'>SIGNUP</a>
+  <a href='/login'>LOG IN</a>
+  </div>
+</nav>
+
 <script>
     import "../app.css";
   </script>
   
-  
+
   <slot />
   
