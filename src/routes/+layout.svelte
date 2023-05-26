600b42c2632305a0f7d070937b34b1775d78289e
LimShuonle <srxmax161@gmail.com>
2023-05-26 15:51:16 +0800

    added login page, may continue to configure(?)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,6 @@
 <script>
     import "../app.css";
   </script>
-  
 
   <slot />
   
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+    import { authenticateUser} from "../../utils/auth";
+    import { goto } from '$app/navigation'
+
+    async function signIn(evt) {
+        evt.preventDefault();
+
+        const result = {
+        username: evt.target['username'].value,
+        password: evt.target['password'].value, 
+        }
+
+
+        const res = await authenticateUser(result.username, result.password)
+        if (res.success) {
+            goto('/')
+        }
+    }
+    
+    
+</script>
+
+<h1 class="text-center text-2xl mt-5 font-bold sm:text-3xl">Login</h1>
+<div class="flex justify-center items-center mt-8">
+    <form on:submit={signIn} class="w-1/3">
+	<div class="form-control w-full">
+		<label class="label" for="username">
+			<span class="label-text text-xl">Username</span>
+		</label>
+		<input
+			type="text"
+			name="username"
+			placeholder="Type here"
+			class="input input-bordered w-full"
+			required
+		/>
+	</div>
+	<div class="form-control w-full">
+		<label class="label" for="password">
+			<span class="label-text text-xl">Password</span>
+		</label>
+		<input
+			type="password"
+			name="password"
+			placeholder="Please insert your password"
+			class="input input-bordered w-full"
+			required
+		/>
+	</div>
+    <div class="form-control w-full mt-4">
+		<button class="btn" on:submit={authenticateUser}>Log In</button>
+	</div>
+</form>
+</div>
